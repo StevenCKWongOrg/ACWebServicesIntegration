817c6417a44f3b1c53290e26a03b94ee393141ca
Allan PM <tm1-a1@stevenckwong.com>
2018-02-07 14:14:24 +1100

    US147 : Get Display Name of user given an API Key
    In-Progress
    Created MyUtility to handle utility capabilities.
    
    Task-Url: https://rally1.rallydev.com/#/196111739164u/detail/userstory/196121113620

---
+++ src/main/resources/com/stevenckwong/ACWebServicesIntegration/MyUtility.java
@@ -0,0 +1,61 @@
+package com.stevenckwong.ACWebServicesIntegration;
+
+import java.io.IOException;
+import java.net.URI;
+import com.rallydev.rest.RallyRestApi;
+
+public class MyUtility {
+	
+	public MyUtility() {
+		
+	}
+	// This method parses the Rally result from logging in and returns the DisplayName
+	// field.
+	public String parseResultForDisplayName(String result) {
+		int startIndex = result.indexOf("\"_refObjectName\": \"");
+		startIndex+=19;
+		
+		int endIndex = result.indexOf("\"", startIndex);
+		
+		String displayName = result.substring(startIndex, endIndex);
+		return displayName;
+	}
+	
+	// This method parses the Rally result from logging in and returns the First name field
+	public String parseResultForFirstName(String result) {
+		return "Stubbed First Name";
+	}
+	
+	
+	public RallyRestApi connectToRallyUsingAPIKey(String apiKey) {
+		URI uri = URI.create("https://rally1.rallydev.com");
+		RallyRestApi rally = new RallyRestApi(uri,apiKey);
+		
+		return rally;
+	}
+	
+	public String logTestRunResult(String testID, String testResult, String buildNumber) {
+		
+		String apiKey = "_CGJbIEnhQDq45u70AWVPFcMsEmGCkO6tZEhYDyg5Dw";
+
+		URI uri = URI.create("https://rally1.rallydev.com");
+		
+		// RallyRestApi rally = new RallyRestApi(uri, username, password);
+		RallyRestApi rally = new RallyRestApi(uri, apiKey);
+		
+//		String QueryString = "(UserName%20%3D%20"+username+")&start=1&pagesize=20";
+//		String queryURL = "/user?query=" + QueryString + "&order=";		
+
+		try {
+			rally.close();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		// Updated this to test out the integration with Rally
+		
+		return "Success";
+	}
+}
