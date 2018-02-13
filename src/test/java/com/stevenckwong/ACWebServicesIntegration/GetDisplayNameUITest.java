9a218f797170a86d71783fa56de99b31b0c16034
Allan PM <tm1-a1@stevenckwong.com>
2018-02-14 10:23:50 +1100

    US148: Get First name of user given an API Key
    Task-Url: https://rally1.rallydev.com/#/196111739164u/detail/userstory/196121117804

--- src/test/java/com/stevenckwong/ACWebServicesIntegration/GetDisplayNameUITest.java
+++ src/test/java/com/stevenckwong/ACWebServicesIntegration/GetDisplayNameUITest.java
@@ -20,16 +20,15 @@ public class GetDisplayNameUITest {
 		// during unit test phase in Maven.
 		
 		try {
-			this.runTest();
+			this.testDisplayName();
+			this.testFirstName();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
 	}
 	
-	
-	private void runTest() throws InterruptedException {
-		
+	private WebDriver launchWebDriver() {
 		// Make sure you run "mvn" with "-DChromeDriverLocation=<actual_chromedriver_location>" 
 		
 		// Parameterizing the location of ChromeDriver as some developers are on Mac and some on Windows
@@ -45,6 +44,15 @@ public class GetDisplayNameUITest {
 		System.setProperty("webdriver.chrome.driver", chromeDriverLocation);
 		
 		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost:8080/ACWebServicesIntegration/index.jsp");
+		
+		return driver;
+	}
+	
+	
+	private void testDisplayName() throws InterruptedException {
+		
+		WebDriver driver = this.launchWebDriver();
 		driver.get("http://localhost:8080/ACWebServicesIntegration/index.jsp");
 		
 		WebElement usernameBox = driver.findElement(By.name("username"));
@@ -63,14 +71,60 @@ public class GetDisplayNameUITest {
 		Thread.sleep(3000);
 		WebElement hiddenDisplayName = driver.findElement(By.id("displayName"));
 		String displayName = hiddenDisplayName.getAttribute("value");
-		System.out.println("Display Name: " + displayName);
-		assertEquals("I'm Team Member 1 for B1",displayName);
+		try {
+			assertEquals("I'm Team Member 1 for B1",displayName);
+			// assertEquals("I'm Team Member 1 for B1 -- FAILTHIS ",displayName); // uncomment this line to fail test
+		} catch (org.junit.ComparisonFailure cfe) {
+			System.out.println("======JUNIT TEST FAIL======");
+			System.out.println("Exception Message:");
+			System.out.println(cfe.getMessage());
+			System.out.println("+++++++++++++++++++++++++++");
+			throw cfe;
+		} finally {
+			driver.close();
+			driver.quit();
+		}		
 		
-		// Thread.sleep(2000);
-		driver.close();
-		driver.quit();
-		// driver.close();
+	}
+	
+	private void testFirstName() throws InterruptedException {
 		
+		WebDriver driver = this.launchWebDriver();
+		driver.get("http://localhost:8080/ACWebServicesIntegration/index.jsp");
+		
+		WebElement usernameBox = driver.findElement(By.name("username"));
+		usernameBox.clear();
+		usernameBox.sendKeys("tm1-b1@stevenckwong.com");
+		
+		WebElement apikeyBox = driver.findElement(By.name("apikey"));
+		apikeyBox.clear();
+		apikeyBox.sendKeys("_x3imUPkjQdiySkvNCrs0d03TiJhzCkkEmswt6fpHOao"); // good API Key
+		// apikeyBox.sendKeys("_x3imUPkjQdiySkvNCrs0d03TiJhzCkkEmswt6fpHOxx"); // bad API Key
+		
+		//WebElement loginButton = driver.findElement(By.name("loginbutton"));
+		WebElement loginButton = driver.findElement(By.id("getDisplayNameButton"));
+		loginButton.click();
+		
+		Thread.sleep(3000);
+		
+		WebElement hiddenFirstName = driver.findElement(By.id("firstName"));
+		String firstName = hiddenFirstName.getAttribute("value");
+		System.out.println("First Name: " + firstName);
+		try {
+			assertEquals("TM 1 for Team B1",firstName);
+			// assertEquals("TM 1 for Team B1 -- FAILTHIS",firstName); //uncomment this line to fail the test
+			
+		} catch (org.junit.ComparisonFailure cfe) {
+			System.out.println("======JUNIT TEST FAIL======");
+			System.out.println("Exception Message:");
+			System.out.println(cfe.getMessage());
+			System.out.println("+++++++++++++++++++++++++++");
+			throw cfe;
+		} finally {
+			driver.close();
+			driver.quit();
+		}
+				
 	}
 }
 
