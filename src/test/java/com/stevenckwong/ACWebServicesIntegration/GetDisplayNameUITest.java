35b9a4951bdcfdd1f5cdd0b320998bfaa920215d
Allan PM <tm1-a1@stevenckwong.com>
2018-02-15 12:26:16 +1100

    DE47 Fixed
    Issue due to unhandled JSON null object when a wrong username is provided.

--- src/test/java/com/stevenckwong/ACWebServicesIntegration/GetDisplayNameUITest.java
+++ src/test/java/com/stevenckwong/ACWebServicesIntegration/GetDisplayNameUITest.java
@@ -63,7 +63,23 @@ public class GetDisplayNameUITest {
 		} 
 	}	
 
-
+	@Test
+	public void testGetUserDoesNotExist() {
+		// fail("Not yet implemented");
+		
+		// return;
+		
+		// This is an integration test. Commented out to return a stubbed test for now
+		// while I figure out how to run a JUnit test in integration-test and not have it run 
+		// during unit test phase in Maven.
+		
+		try {
+			this.testUserDoesNotExist();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
 	
 	private WebDriver launchWebDriver() {
 		// Make sure you run "mvn" with "-DChromeDriverLocation=<actual_chromedriver_location>" 
@@ -191,5 +207,41 @@ public class GetDisplayNameUITest {
 		}
 				
 	}	
+
+	private void testUserDoesNotExist() throws InterruptedException {
+		
+		WebDriver driver = this.launchWebDriver();
+		driver.get("http://localhost:8080/ACWebServicesIntegration/index.jsp");
+		
+		WebElement usernameBox = driver.findElement(By.name("username"));
+		usernameBox.clear();
+		usernameBox.sendKeys("user@doesnotexist.com");
+		
+		WebElement apikeyBox = driver.findElement(By.name("apikey"));
+		apikeyBox.clear();
+		apikeyBox.sendKeys("_x3imUPkjQdiySkvNCrs0d03TiJhzCkkEmswt6fpHOao"); // good API Key
+		// apikeyBox.sendKeys("_x3imUPkjQdiySkvNCrs0d03TiJhzCkkEmswt6fpHOxx"); // bad API Key
+		
+		//WebElement loginButton = driver.findElement(By.name("loginbutton"));
+		WebElement loginButton = driver.findElement(By.id("getDisplayNameButton"));
+		loginButton.click();
+		
+		Thread.sleep(3000);
+		
+		WebElement bUserDoesNotExistElement = driver.findElement(By.id("userDoesNotExist"));
+		String bUserDoesNotExist = bUserDoesNotExistElement.getAttribute("value");
+		try {
+			assertEquals("true",bUserDoesNotExist);
+			// assertEquals("TM 1 for Team B1 -- FAILTHIS",firstName); //uncomment this line to fail the test
+			
+		} catch (org.junit.ComparisonFailure cfe) {
+			throw cfe;
+		} finally {
+			driver.close();
+			driver.quit();
+		}
+				
+	}
+
 }
 
